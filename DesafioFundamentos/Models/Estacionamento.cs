fc0abac888934ccc8431aff330e829670f9b5cfb
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2022-11-14 23:24:17 -0300

    e

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -22,7 +22,6 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
-
             Console.WriteLine("Digite a placa do veículo para remover:");
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa 
